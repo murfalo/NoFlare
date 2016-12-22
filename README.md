17e080eb2a67bdfda54976c887524736731e255e
Murphy Angelo <murphycangelo@u.northwestern.edu>
2016-12-22 13:03:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# No Flare
---
+++ src/NoFlare.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace NoFlare
+{
+  class NoFlare : Mod
+  {
+    public NoFlare()
+    {
+      Properties = new ModProperties() { Autoload = false };
+    }
+  }
+}
