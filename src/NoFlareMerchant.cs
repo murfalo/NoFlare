bcecc6408bb421e96bf629b33bad51bfb23102ce
Murphy Angelo <murphycangelo@u.northwestern.edu>
2016-12-23 23:06:28 -0500

    Merchant now sells Flare Gun and flares

---
+++ src/NoFlareMerchant.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoFlare
+{
+  public class NoFlareMerchant : GlobalNPC
+  {
+    public override void SetupShop(int type, Chest shop, ref int nextSlot)
+    {
+      if (type != NPCID.Merchant) return;
+      shop.item[nextSlot++].SetDefaults(930);
+      shop.item[nextSlot++].SetDefaults(931);
+      shop.item[nextSlot++].SetDefaults(1614);
+    }
+  }
+}
